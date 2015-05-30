c2a26987642c3304e2b509f435eb33f8de57d621
fujimuramasa <noctis.yin@gmail.com>
2015-05-30 14:10:54 +0100

    Revert "Revert "basic layout of parser""
    This reverts commit 198c24ca674058ca38dced150a77bd3bc88f1cfd.

--- karyotype-parser/project.clj
+++ karyotype-parser/project.clj
@@ -1,9 +1,11 @@
 (defproject karyotype-parser "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Karyotype-parser prototype"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+  				;[ncl.karyotype "1.0.0-SNAPSHOT"]
+  				]
   :main ^:skip-aot karyotype-parser.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- karyotype-parser/src/karyotype_parser/core.clj
+++ karyotype-parser/src/karyotype_parser/core.clj
@@ -1,7 +1,106 @@
 (ns karyotype-parser.core
-  (:gen-class))
+	(:require 
+		[clojure.string :as str]
+		)
+  	(:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;check for "+",return details like ("+3" "+7").
+(defn Plus [karyotype]
+	(re-seq #"\+[\d X Y]*" karyotype))
+
+;check for "-",return details like ("-3" "-7").
+(defn Minus [karyotype]
+	(re-seq #"\-[\d X Y]*" karyotype))
+
+;check for Fail.
+(defn Fail? [karyotype]
+	(if (re-find #"Fail" karyotype)
+		'True
+		'False
+	))
+
+;check for Idem (stemline karyotype in a subclone).
+(defn Idem? [karyotype]
+	(if (re-find #"idem" karyotype)
+		'True
+		'False
+	))
+
+;check for add (additional material of unkown origin).
+;(defn Add? [karyotype]
+	;(if (re-find #"add" karyotype)
+		;'True
+		;'False
+	;))
+
+;check for +mar (mark chromosome).
+(defn Mar? [karyotype]
+	(if (re-find #"\+mar" karyotype)
+		'True
+		'False
+	))
+
+;check for inc (incomplete karyotype).
+(defn Inc? [karyotype]
+	(if (re-find #"inc" karyotype)
+		'True
+		'False
+	))
+
+;Dash
+
+(defn Extr-loc [exp]
+	(str/join (re-seq #"[\d p q]" exp))
+)
+
+(defn Extr-mult-loc [exp]
+	(let [not-empty? (complement empty?)
+		coll (into [] (filter not-empty? (re-seq #"[\d,p,q]*" exp)))
+		len (count coll)]
+			(map (fn [x] (str/join [(nth coll x) (nth coll (+ x (/ len 2)))])) (range (/ len 2)))
+	)
+)
+
+;check for del.
+(defn Del [karyotype]
+	(let [sub (re-seq #"del[\d p q \( \)]*\)" karyotype)] 
+		(map Extr-loc sub)
+	)
+)
+
+;check for add.
+(defn Add [karyotype]
+	(let [sub (re-seq #"add[\d p q \( \)]*\)" karyotype)] 
+		(map Extr-loc sub)
+	)
+)
+
+;check for dup.
+(defn Dup [karyotype]
+	(let [sub (re-seq #"dup[\d p q \( \)]*\)" karyotype)] 
+		(->> sub
+			;convert "dup" to "duP" in strings to avoid misunderstanding because of "p".
+			(map (fn [expr] (str/replace expr #"up" "uP")))
+			(map Extr-loc)
+		)
+	)	
+)
+
+;check for trp.
+(defn Trp [karyotype]
+	(let [sub (re-seq #"trp[\d p q \( \)]*\)" karyotype)] 
+		(->> sub
+			;convert "trp" to "trP" in strings to avoid misunderstanding because of "p".
+			(map (fn [expr] (str/replace expr #"rp" "rP")))
+			(map Extr-loc)
+		)
+	)	
+)
+
+;check for i.
+(defn Iso [karyotype]
+	(let [sub (re-seq #"i[\d p q \( \)]*\)" karyotype)] 
+		(map Extr-loc sub)
+	)
+)
+
