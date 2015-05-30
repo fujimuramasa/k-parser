198c24ca674058ca38dced150a77bd3bc88f1cfd
fujimuramasa <noctis.yin@gmail.com>
2015-05-30 14:10:01 +0100

    Revert "basic layout of parser"
    This reverts commit 8a2db522f0830f67f714422f88d103468b6a9981.

--- karyotype-parser/project.clj
+++ karyotype-parser/project.clj
@@ -1,11 +1,9 @@
 (defproject karyotype-parser "0.1.0-SNAPSHOT"
-  :description "Karyotype-parser prototype"
+  :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-  				;[ncl.karyotype "1.0.0-SNAPSHOT"]
-  				]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot karyotype-parser.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
