70872063dff437a0628098f639a2efdaa5b5ee0c
fujimuramasa <noctis.yin@gmail.com>
2015-05-30 16:55:46 +0100

    anyway it works again

--- karyotype-parser/project.clj
+++ karyotype-parser/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[uk.org.russet/tawny-owl "1.3.0"]
+  				[org.clojure/clojure "1.6.0"]
+  				[ncl.karyotype "1.0.0-SNAPSHOT"]]
   :main ^:skip-aot karyotype-parser.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
