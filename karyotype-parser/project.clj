4a9e624560180a08099fc6e22140a19f0fe53a15
fujimuramasa <noctis.yin@gmail.com>
2015-06-25 10:41:32 +0100

    20150625

--- karyotype-parser/project.clj
+++ karyotype-parser/project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[uk.org.russet/tawny-owl "1.3.0"]
   				[org.clojure/clojure "1.6.0"]
-  				[ncl.karyotype "1.0.0-SNAPSHOT"]]
+  				[ncl.karyotype "1.0.0"]]
   :main ^:skip-aot karyotype-parser.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
+
+  
